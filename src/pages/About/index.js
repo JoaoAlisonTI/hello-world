bc92919a50e9629d275ed877b330b5e7870ac5fc
JoaoAlisonTI <joaoalison.ti@gmail.com>
2024-07-09 17:02:17 -0300

    style: Adicionando estilização a página About

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,5 +1,25 @@
+import styles from "./About.module.css";
+import { PostModel } from 'components/PostModel'
+import fotoCapa from 'assets/sobre_mim_capa.png'
+
 export function About() {
-    return (
-        <h1>Página sobre...</h1>
-    )
+  return (
+    <PostModel 
+      fotoCapa={fotoCapa}
+      titulo="Sobre"
+    >
+      <h3 className={styles.subtitulo}>Olá, eu sou o Antônio!
+      </h3>
+
+      <img
+        src="https://github.com/JoaoAlisonTI.png"
+        alt="Foto de perfil do Github"
+        className={styles.fotoSobreMim}
+      />
+
+      <p className={styles.paragrafo}>
+        Oi, tudo bem? Eu sou instrutor de Front-end na Alura e estou feliz de te ver por aqui.
+      </p>
+    </PostModel>
+  )
 }

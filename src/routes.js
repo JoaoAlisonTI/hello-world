71028d38ce4b603b28d380541ea3f273b788a2ca
JoaoAlisonTI <joaoalison.ti@gmail.com>
2024-07-09 16:23:03 -0300

    feat: Adicionando DefaultPage

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { DefaultPage } from 'components/DefaultPage';
 import { Menu } from 'components/Menu'
 import { Home } from './pages/Home';
 import { About } from './pages/About';
@@ -9,8 +10,11 @@ export function RoutesComponent() {
       <Menu />
       
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/" element={<DefaultPage />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
+
         <Route path="*" element={<div>Página não encontrada</div>} />
       </Routes>
     </BrowserRouter>
